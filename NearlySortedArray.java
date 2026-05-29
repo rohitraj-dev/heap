a10dad62411f5cb6b05d5552387b603225542ed6
Rohit Raj <rajrohit7388@outlook.com>
2026-05-29 20:24:22 +0530

    Add files via upload

---
+++ NearlySortedArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class NearlySortedArray
+{
+    public void nearlySorted(int[] arr, int k)
+    {
+        int idx = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int ele : arr)
+        {
+            pq.add(ele);
+            if(pq.size()>k)
+                arr[idx++] = pq.remove();
+        }
+        while(pq.size()>0)
+            arr[idx++] = pq.remove();
+    }
+}
